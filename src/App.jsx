54d509d033b16c3a1c5a8cde6c43dfc4f136675f
Supanut1911 <ynotmela@gmail.com>
2023-02-16 11:59:10 +0700

    initial-and-install-lib

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { InfinitePeople } from "./people/InfinitePeople";
+import { InfiniteSpecies } from "./species/InfiniteSpecies";
+import { QueryClient } from "@tanstack/react-query";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Infinite SWAPI</h1>
+      <InfinitePeople />
+      {/* <InfiniteSpecies /> */}
+    </div>
+  );
+}
+
+export default App;
