4904522814bbf0459b2099c6b02c4230cb44a198
Supanut1911 <ynotmela@gmail.com>
2023-03-18 20:19:03 +0700

    fix-bug-cant-call-queryFn-cause-need-to-add-queryKey-be-array

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,38 @@ import { InfinitePeople } from "./people/InfinitePeople";
 import { InfiniteSpecies } from "./species/InfiniteSpecies";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useEffect } from "react";
 
 const queryClient = new QueryClient();
 
+const fetchUrl = async (url) => {
+  const initialUrl = "https://swapi.dev/api/people";
+
+  const response = await fetch(initialUrl);
+  return response.json();
+};
+
 function App() {
+  // useEffect(() => {
+  //   const initialUrl = "https://swapi.dev/api/people";
+
+  //   fetch(initialUrl)
+  //     .then((res) => res.json())
+  //     .then((data) => console.log("data", data));
+  // }, []);
+
+  const fetchUrl = async (url) => {
+    const initialUrl = "https://swapi.dev/api/people";
+
+    const response = await fetch(initialUrl);
+    return await response.json();
+  };
+
+  useEffect(() => {
+    const x = fetchUrl("");
+    // console.log("data=>", x);
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <div className="App">
--- src/people/InfinitePeople.jsx
+++ src/people/InfinitePeople.jsx
@@ -1,13 +1,59 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { Person } from "./Person";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
 
 const initialUrl = "https://swapi.dev/api/people/";
-const fetchUrl = async (url) => {
-  const response = await fetch(url);
-  return response.json();
-};
 
 export function InfinitePeople() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const fetchUrl = async (url) => {
+    console.log("called");
+    const response = await fetch(url);
+    return response.json();
+  };
+
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+    isFetching,
+    isFetchingNextPage,
+    isError,
+    error,
+  } = useInfiniteQuery(
+    ["sw-people"],
+    ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    {
+      getNextPageParam: (lastPage) => lastPage.next || undefined,
+    }
+  );
+
+  if (isLoading) {
+    return <div className="loading">Loading ...</div>;
+  }
+  if (isError) {
+    return <div>Error! {error.toString()}</div>;
+  }
+
+  return (
+    <>
+      {isFetching && <div className="loading">Fetching</div>}
+
+      <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+        {data.pages.map((pageData) => {
+          return pageData.results.map((person) => {
+            return (
+              <Person
+                key={person.name}
+                name={person.name}
+                hairColor={person.hairColor}
+                eyeColor={person.eyeColor}
+              />
+            );
+          });
+        })}
+      </InfiniteScroll>
+    </>
+  );
 }
